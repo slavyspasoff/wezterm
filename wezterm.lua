da093839acdfb4043a10ae777053d366f839b172
Slavy Spasoff <slavyspasoff@gmail.com>
2023-12-20 19:12:15 +0100

    update Geist Mono as fallback

--- wezterm.lua
+++ wezterm.lua
@@ -7,18 +7,19 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- fonts
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 400 })
--- config.font = wezterm.font("Victor Mono", { weight = 500 })
 config.font_size = 12.5
+-- config.font = wezterm.font("Geist Mono", { weight = 400 })
+-- config.font_size = 13
 config.freetype_load_target = "HorizontalLcd"
 
 -- color scheme
 for k, v in pairs(colorscheme) do
-	config[k] = v
+  config[k] = v
 end
 config.command_palette_bg_color = "#161925"
 config.bold_brightens_ansi_colors = false
@@ -36,10 +37,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.initial_rows = 20
 config.initial_cols = 105
 config.window_padding = {
-	left = 8,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 8,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 -- anti-aliasing
 config.anti_alias_custom_block_glyphs = true
